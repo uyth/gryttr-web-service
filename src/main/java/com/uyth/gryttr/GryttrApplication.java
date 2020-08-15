d99c426df5f979f1d40924612112f4a77d15fa3f
uy <uyth95@gmail.com>
2020-08-15 14:09:34 +0200

    Init project

---
+++ src/main/java/com/uyth/gryttr/GryttrApplication.java
@@ -0,0 +1,13 @@
+package com.uyth.gryttr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GryttrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GryttrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uyth/gryttr/GryttrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uyth.gryttr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GryttrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
