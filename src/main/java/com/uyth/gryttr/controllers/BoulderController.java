a4108827a6d697a9733f4536228a74ef61b645db
uyth <uyth95@gmail.com>
2020-08-15 20:26:57 +0200

    Add API for finding boulders and collections by id

--- src/main/java/com/uyth/gryttr/controllers/BoulderController.java
+++ src/main/java/com/uyth/gryttr/controllers/BoulderController.java
@@ -1,9 +1,12 @@
 package com.uyth.gryttr.controllers;
 
+import com.uyth.gryttr.exceptions.ResourceNotFoundException;
 import com.uyth.gryttr.model.Boulder;
 import com.uyth.gryttr.repository.BoulderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,13 @@ public class BoulderController {
     @GetMapping("/boulders")
     public List<Boulder> getAllBoulder() {
         return boulderRepository.findAll();
+    }
+
+    @GetMapping("/boulders/{id}")
+    public ResponseEntity<Boulder> getBoulderById(@PathVariable("id") Long id) throws ResourceNotFoundException {
+        Boulder boulder = boulderRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Boulder with ID '" + id.toString() + "' was not found."));
+        return ResponseEntity.ok().body(boulder);
     }
 
 }
--- src/main/java/com/uyth/gryttr/controllers/CollectionController.java
+++ src/main/java/com/uyth/gryttr/controllers/CollectionController.java
@@ -1,9 +1,12 @@
 package com.uyth.gryttr.controllers;
 
+import com.uyth.gryttr.exceptions.ResourceNotFoundException;
 import com.uyth.gryttr.model.Collection;
 import com.uyth.gryttr.repository.CollectionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,13 @@ public class CollectionController {
     @GetMapping("/collections")
     public List<Collection> getAllBoulder() {
         return collectionRepository.findAll();
+    }
+
+    @GetMapping("/collections/{id}")
+    public ResponseEntity<Collection> getCollectionById(@PathVariable("id") Long id) throws ResourceNotFoundException {
+        Collection collection = collectionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Collection with ID '" + id.toString() + "' was not found."));
+        return ResponseEntity.ok().body(collection);
     }
 
 }
